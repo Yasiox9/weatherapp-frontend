f6ba444576164aeda1b97fcc4c841b2ed0be1e03
Yacine <Yacine.lariane@gmail.com>
2023-06-26 16:58:37 +0200

    test

--- login.js
+++ login.js
@@ -6,7 +6,7 @@ document.querySelector("#register").addEventListener("click", function () {
         email : document.querySelector("#registerEmail").value,
         password : document.querySelector("#registerPassword").value,
     }
-    fetch("weatherapp-backend-jade.vercel.app/users/signup", {
+    fetch("https://weatherapp-backend-jade.vercel.app/users/signup", {
         method: "POST",
         headers: {"Content-Type" : "application/json"},
         body: JSON.stringify(user),
@@ -25,7 +25,7 @@ document.querySelector("#connection").addEventListener("click", function()  {
         email: document.querySelector("#connectionEmail").value,
         password: document.querySelector("#connectionPassword").value,
     }
-    fetch("weatherapp-backend-jade.vercel.app/users/signin",{
+    fetch("https://weatherapp-backend-jade.vercel.app/users/signin",{
         method: "POST",
         headers: {"Content-Type" : "application/json"},
         body: JSON.stringify(user),
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch('weatherapp-backend-jade.vercel.app/weather')
+fetch('https://weatherapp-backend-jade.vercel.app/weather')
 	.then(response => response.json())
 	.then(data => {
 		if (data.weather) {
@@ -24,7 +24,7 @@ fetch('weatherapp-backend-jade.vercel.app/weather')
 function updateDeleteCityEventListener() {
 	for (let i = 0; i < document.querySelectorAll('.deleteCity').length; i++) {
 		document.querySelectorAll('.deleteCity')[i].addEventListener('click', function () {
-			fetch(`weatherapp-backend-jade.vercel.app/weather/${this.id}`, { method: 'DELETE' })
+			fetch(`https://weatherapp-backend-jade.vercel.app/weather/${this.id}`, { method: 'DELETE' })
 				.then(response => response.json())
 				.then(data => {
 					if (data.result) {
@@ -38,7 +38,7 @@ function updateDeleteCityEventListener() {
 document.querySelector('#addCity').addEventListener('click', function () {
 	const cityName = document.querySelector('#cityNameInput').value;
 
-	fetch('weatherapp-backend-jade.vercel.app/weather', {
+	fetch('https://weatherapp-backend-jade.vercel.app/weather', {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify({ cityName }),
