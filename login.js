513fea91ff1028feb3f0582e2a7aae0818693cc5
Yacine <Yacine.lariane@gmail.com>
2023-06-26 16:34:23 +0200

    weather app front end

---
+++ login.js
@@ -0,0 +1,39 @@
+// Insert your code here
+// inscription 
+document.querySelector("#register").addEventListener("click", function () {
+    const user = {
+        name : document.querySelector("#registerName").value,
+        email : document.querySelector("#registerEmail").value,
+        password : document.querySelector("#registerPassword").value,
+    }
+    fetch("weatherapp-backend-jade.vercel.app/users/signup", {
+        method: "POST",
+        headers: {"Content-Type" : "application/json"},
+        body: JSON.stringify(user),
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data.result){ response => response.json(true);
+            window.location.assign("index.html");
+        }
+    });
+});
+
+//connection
+document.querySelector("#connection").addEventListener("click", function()  {
+    const user = {
+        email: document.querySelector("#connectionEmail").value,
+        password: document.querySelector("#connectionPassword").value,
+    }
+    fetch("weatherapp-backend-jade.vercel.app/users/signin", {
+        method: "POST",
+        headers: {"Content-Type" : "application/json"},
+        body: JSON.stringify(user),
+    })
+    .then(response => response.json())
+    .then(data => {
+        if(data.result){ response => response.json(true);
+            window.location.assign("index.html");
+        }
+        })
+})
